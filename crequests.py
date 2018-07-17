4dd51fcdc6e850542161c31b400656cdd8ce64d4
Shamik ray <shamik.ray@coviam.com>
2018-07-18 01:07:23 +0530

    Futures request

--- crequests.py
+++ crequests.py
@@ -7,6 +7,8 @@ Wrapper over requests library to provide utility stuff.
 
 from requests import Session
 from requests import adapters
+from requests_futures.sessions import FuturesSession
+
 
 CONNECT_TIMEOUT = 5.0
 READ_TIMEOUT = 15.0
@@ -18,7 +20,7 @@ POOL_MAXSIZE = 1000
 class CRequests:
 
     def __init__(self, CONNECT_TIMEOUT=CONNECT_TIMEOUT, READ_TIMEOUT=READ_TIMEOUT, stream=False):
-        self.requests = Session()
+        self.requests = FuturesSession()
         self.requests.stream = stream
         self.requests.trust_env = False
         self.requests.mount('http://', adapters.HTTPAdapter(pool_connections=NUM_POOLS,
@@ -32,35 +34,44 @@ class CRequests:
 
     def request(self, method, url, **kwargs):
         kwargs.setdefault('timeout', self.tuple)
-        return self.requests.request(method, url, **kwargs)
+        future = self.requests.request(method, url, **kwargs)
+        return future.result(timeout=READ_TIMEOUT)
 
     def get(self, url, **kwargs):
         kwargs.setdefault('timeout', self.tuple)
-        return self.requests.get(url, **kwargs)
+        future=self.requests.get(url, **kwargs)
+        return future.result(timeout=READ_TIMEOUT)
 
     def options(self, url, **kwargs):
         kwargs.setdefault('timeout', self.tuple)
-        return self.requests.options(url, **kwargs)
+        future= self.requests.options(url, **kwargs)
+        return future.result(timeout=READ_TIMEOUT)
 
     def head(self, url, **kwargs):
         kwargs.setdefault('timeout', self.tuple)
-        return self.requests.head(url, **kwargs)
+        future= self.requests.head(url, **kwargs)
+        return future.result(timeout=READ_TIMEOUT)
 
     def post(self, url, data=None, json=None, **kwargs):
         kwargs.setdefault('timeout', self.tuple)
-        return self.requests.post(url, data=data, json=json, **kwargs)
+        future= self.requests.post(url, data=data, json=json, **kwargs)
+        return future.result(timeout=READ_TIMEOUT)
 
     def put(self, url, data=None, **kwargs):
         kwargs.setdefault('timeout', self.tuple)
-        return self.requests.put(url, data=data, **kwargs)
+        future= self.requests.put(url, data=data, **kwargs)
+        return future.result(timeout=READ_TIMEOUT)
 
     def patch(self, url, data=None, **kwargs):
         kwargs.setdefault('timeout', self.tuple)
-        return self.requests.patch(url, data=data, **kwargs)
+        future= self.requests.patch(url, data=data, **kwargs)
+        return future.result(timeout=READ_TIMEOUT)
 
     def delete(self, url, **kwargs):
         kwargs.setdefault('timeout', self.tuple)
-        return self.requests.delete(url, **kwargs)
+        future= self.requests.delete(url, **kwargs)
+        return future.result(timeout=READ_TIMEOUT)
+
 
     def close(self):
         self.requests.close()
